2748b516bc0bc5972c8cf538261e0e4813c23d5d
nitin <nitin4real@gmail.com>
2024-11-19 00:31:30 +0530

    update endpoint to new servers

--- src/constants/apiEndpoints.ts
+++ src/constants/apiEndpoints.ts
@@ -1,4 +1,5 @@
 export const ENPOINTS = {
-    BASE_URL: 'https://nitinsingh.in:3012',
+    // BASE_URL: 'https://nitinsingh.in:3012',
     // BASE_URL: 'http://localhost:3013',
+    BASE_URL: 'https://agoraaidemo.in:3012',
 }
