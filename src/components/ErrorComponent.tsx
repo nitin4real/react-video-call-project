a10af2a66d6fb3e0198c61bddb15dd0bd0e32c3a
nitin <nitin4real@gmail.com>
2024-12-13 18:34:56 +0530

    error msg update

--- src/components/ErrorComponent.tsx
+++ src/components/ErrorComponent.tsx
@@ -2,7 +2,14 @@ import errorImage from '../images/error.jpg'
 export const ErrorComponent = ({ message }: { message: string; }) => {
     return (
         <div className="error-container">
-            <p className="error-message">Error: {message}</p>
+            <p className="error-message">Error: {message}
+            <br></br>
+            This might be because you are using a invalid channel name.
+            <br></br>
+            Please confirm the channel name with the team.
+            <br></br>
+            Only specific channels are allowed to be created.
+            </p>
             <img src={errorImage} alt="Error in rendering error image((😂))"/>
         </div>
     );
